79027b3f771efe78495324f8999fdadc481183a2
koya-ken <koya.smp@gmail.com>
2019-10-27 18:21:30 +0900

    moved coco to https://github.com/koya-ken/cocoparser

--- coco/read.py
+++
@@ -1,7 +0,0 @@
-import msgpack
-
-
-def t():
-    with open('data.msgpack', 'rb') as f:
-        data = msgpack.unpack(f, raw=False)
-    # print(data['images'])
--- coco/textutil.py
+++
@@ -1,3 +0,0 @@
-def dumplist(list_data, filepath):
-    with open(filepath,'w') as f:
-        f.writelines(map(lambda x: str(x),list_data))
